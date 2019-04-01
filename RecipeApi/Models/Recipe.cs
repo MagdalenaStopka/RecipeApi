cee5b541fff332bc07ba4c8637ae8288502b6495
Magdalena Stopka <m.stopka@microsoft.wsei.edu.pl>
2019-04-01 19:06:58 +0200

    Add project files.

---
+++ RecipeApi/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeApi.Models
+{
+    public class Recipe
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+
+        public string Image { get; set; }
+
+        public string Body { get; set; }
+
+    }
+}
